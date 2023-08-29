71da6cc2e91d9a4da2d95d15467c6e3752aba7c9
Ucheizzzy <uchechukwuobiakor@gmail.com>
2023-08-30 00:13:12 +0100

    unsplash images using react query

---
+++ src/context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+const AppContext = createContext()
+
+//check for the initial mode of the browser
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches
+  const storedDarkMode = localStorage.getItem('darkTheme') === 'true'
+
+  return storedDarkMode || prefersDarkMode
+}
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkThem] = useState(getInitialDarkMode())
+  const [searchTerm, setSearchTerm] = useState('cat')
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme
+    setIsDarkThem(newDarkTheme)
+    localStorage.setItem('darkTheme', newDarkTheme)
+  }
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkTheme)
+  }, [isDarkTheme])
+
+  return (
+    <AppContext.Provider
+      value={{ toggleDarkTheme, isDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
